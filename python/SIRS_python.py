4b672e284de048ac67b485aacd0603e0bd2e2e5e
Sarah <sarah.kramer.374@gmail.com>
2019-12-30 17:04:41 +0100

    12.30: Add functions to parallelize
    Former-commit-id: 6721632d22af8f9327b4b67c7bbc92b0a7b16b36

--- python/SIRS_python.py
+++ python/SIRS_python.py
@@ -1,20 +1,77 @@
 import sys
 import numpy as np
-from numba import jit, njit
+from numba import jit, prange
 from scipy import *
 
 
-# SIRS (including air travel and commuting):
-#@njit
-def propagate_SIRS(tmStrt, tmEnd, tmStep, tmRange, S_0, I_0, popN, D_d, L_d, beta_d, airScale_d, Countries, n_count,
-                   airRand):
-    def remove_negatives(mat):
-        i, j = np.where(mat < 0.0)
+# Run full ensemble:
+@jit(nopython=True, nogil=True, parallel=True, cache=True)
+def run_ensemble(tmStrt, tmEnd, tmStep, tmRange, S0, I0, popN, D, L, beta, airScale, Countries, n, airRand, num_ens):
+
+    xprior_temp = np.zeros((num_ens, np.square(n) * 3), dtype=np.float64)
+
+    for i in prange(num_ens):
+        Sr_tmp = propagate_SIRS(tmStrt, tmEnd, tmStep, tmRange, S_0=S0[:, :, i], I_0=I0[:, :, i], popN=popN,
+                                D_d=D[i], L_d=L[i], beta_d=beta[:, :, i], airScale_d=airScale[i],
+                                Countries=Countries, n_count=n, airRand=airRand)
+
+        xprior_temp_i = Sr_tmp[0][Sr_tmp[0].shape[0] - 1].reshape(np.square(n))
+        xprior_temp_i = np.append(xprior_temp_i, Sr_tmp[1][Sr_tmp[1].shape[0] - 1].reshape(np.square(n)))
+        xprior_temp_i = np.append(xprior_temp_i, Sr_tmp[2][Sr_tmp[2].shape[0] - 1].reshape(np.square(n)))
+        xprior_temp[i] = xprior_temp_i
+
+    return np.transpose(xprior_temp)
+
+
+@jit(nopython=True, nogil=True, parallel=True, cache=True)
+def run_forecast(tmStrt, tmEnd, tmStep, tmRange, S0, I0, popN, D, L, beta, airScale, Countries, n,
+                 airRand, num_ens, nfc, tm_step):
+
+    fcast_temp = np.zeros((np.square(n) * 3, num_ens, nfc), dtype=np.float64)
+
+    for i in prange(num_ens):
+        Sr_tmp = propagate_SIRS(tmStrt, tmEnd, tmStep, tmRange, S_0=S0[:, :, i], I_0=I0[:, :, i], popN=popN,
+                                D_d=D[i], L_d=L[i], beta_d=beta[:, :, i], airScale_d=airScale[i],
+                                Countries=Countries, n_count=n, airRand=airRand)
+
+        statesresS = np.empty((nfc, np.square(n)))
+        statesresI = np.empty((nfc, np.square(n)))
+        statesresnewI = np.empty((nfc, np.square(n)))
+        prev_newI = Sr_tmp[2][0].reshape(np.square(n))
+
+        for j in range(0, nfc):
+            k = tm_step * (j + 1)
+            statesresS[j] = Sr_tmp[0][k].reshape(np.square(n))
+            statesresI[j] = Sr_tmp[1][k].reshape(np.square(n))
+
+            curr_newI = Sr_tmp[2][k].reshape(np.square(n))
+            statesresnewI[j] = curr_newI - prev_newI
+            prev_newI = curr_newI
+
+        fcast_temp_i = np.transpose(statesresS)
+        fcast_temp_i = np.append(fcast_temp_i, np.transpose(statesresI), axis=0)
+        fcast_temp_i = np.append(fcast_temp_i, np.transpose(statesresnewI), axis=0)
+
+        fcast_temp[:, i] = fcast_temp_i
+
+    return fcast_temp
+
+
+@jit(nopython=True, nogil=True, cache=True)
+def remove_negatives(mat):
+    i, j = np.where(mat < 0.0)
+    if len(i) > 0:
         for ix in i:
             for jx in j:
                 mat[ix, jx] = 0.0
-        return mat
+    return mat
 
+
+# SIRS (including air travel and commuting):
+@jit(nopython=True, nogil=True, cache=True)
+# @njit
+def propagate_SIRS(tmStrt, tmEnd, tmStep, tmRange, S_0, I_0, popN, D_d, L_d, beta_d, airScale_d, Countries, n_count,
+                   airRand):
     cnt = 0
 
     # print(tmStrt)
@@ -34,15 +91,15 @@ def propagate_SIRS(tmStrt, tmEnd, tmStep, tmRange, S_0, I_0, popN, D_d, L_d, bet
     # n_count = np.float64(n_count)
     # tm_sz = np.float64(tm_sz)
 
-    S_list = np.empty((n_count, n_count, tm_sz), dtype=np.float64)
-    S_list[:, :, 0] = S_0
+    S_list = np.empty((tm_sz, n_count, n_count), dtype=np.float64)
+    S_list[0] = S_0
     # print(S_list.shape)
 
-    I_list = np.empty((n_count, n_count, tm_sz), dtype=np.float64)
-    I_list[:, :, 0] = I_0
+    I_list = np.empty((tm_sz, n_count, n_count), dtype=np.float64)
+    I_list[0] = I_0
 
-    newI_list = np.empty((n_count, n_count, tm_sz), dtype=np.float64)
-    newI_list[:, :, 0] = np.zeros((n_count, n_count))
+    newI_list = np.empty((tm_sz, n_count, n_count), dtype=np.float64)
+    newI_list[0] = np.zeros((n_count, n_count), dtype=np.float64)
 
     for t in tm_vec:
 
@@ -53,31 +110,32 @@ def propagate_SIRS(tmStrt, tmEnd, tmStep, tmRange, S_0, I_0, popN, D_d, L_d, bet
         # print(t_true) # pretty sure this is correct! first value of 2010-11 corresponds to third day of October
 
         if t_true in range(0, 31) or t_true in range((0 + 365), (31 + 365)):
-            airRand_temp = airRand[:, :, 0]
+            airRand_temp = airRand[0]
         elif t_true in range(31, 59) or t_true in range((31 + 365), (59 + 365)):
-            airRand_temp = airRand[:, :, 1]
+            airRand_temp = airRand[1]
         elif t_true in range(59, 90) or t_true in range((59 + 365), (90 + 365)):
-            airRand_temp = airRand[:, :, 2]
+            airRand_temp = airRand[2]
         elif t_true in range(90, 120) or t_true in range((90 + 365), (120 + 365)):
-            airRand_temp = airRand[:, :, 3]
+            airRand_temp = airRand[3]
         elif t_true in range(120, 151) or t_true in range((120 + 365), (151 + 365)):
-            airRand_temp = airRand[:, :, 4]
+            airRand_temp = airRand[4]
         elif t_true in range(151, 181) or t_true in range((151 + 365), (181 + 365)):
-            airRand_temp = airRand[:, :, 5]
+            airRand_temp = airRand[5]
         elif t_true in range(181, 212) or t_true in range((181 + 365), (212 + 365)):
-            airRand_temp = airRand[:, :, 6]
+            airRand_temp = airRand[6]
         elif t_true in range(212, 243) or t_true in range((212 + 365), (243 + 365)):
-            airRand_temp = airRand[:, :, 7]
+            airRand_temp = airRand[7]
         elif t_true in range(243, 273) or t_true in range((243 + 365), (273 + 365)):
-            airRand_temp = airRand[:, :, 8]
+            airRand_temp = airRand[8]
         elif t_true in range(273, 304) or t_true in range((273 + 365), (304 + 365)):
-            airRand_temp = airRand[:, :, 9]
+            airRand_temp = airRand[9]
         elif t_true in range(304, 334) or t_true in range((304 + 365), (334 + 365)):
-            airRand_temp = airRand[:, :, 10]
+            airRand_temp = airRand[10]
         elif t_true in range(334, 365) or t_true in range((334 + 365), (365 + 365)):
-            airRand_temp = airRand[:, :, 11]
+            airRand_temp = airRand[11]
         else:
-            print('ERROR: Out of seasonal range!')
+            # print('ERROR: Out of seasonal range!')
+            raise Exception('t_true is out of seasonal range')
 
         # Multiply airRand by airScale:
         airRand_temp = airScale_d * airRand_temp
@@ -87,9 +145,9 @@ def propagate_SIRS(tmStrt, tmEnd, tmStep, tmRange, S_0, I_0, popN, D_d, L_d, bet
         # print(cnt)
 
         # Set S, I, newI
-        S = S_list[:, :, cnt - 1]
-        I = I_list[:, :, cnt - 1]
-        newI = newI_list[:, :, cnt - 1]
+        S = S_list[cnt - 1]
+        I = I_list[cnt - 1]
+        newI = newI_list[cnt - 1]
 
         # ### Daytime ###
         # Step 1 #
@@ -530,9 +588,9 @@ def propagate_SIRS(tmStrt, tmEnd, tmStep, tmRange, S_0, I_0, popN, D_d, L_d, bet
         ik4 = smci - smcr + Eitrav
         ik4a = smci  # + Eitrav
 
-        S_list[:, :, cnt] = S + sk1 / 6 + sk2 / 3 + sk3 / 3 + sk4 / 6
-        I_list[:, :, cnt] = I + ik1 / 6 + ik2 / 3 + ik3 / 3 + ik4 / 6
-        newI_list[:, :, cnt] = newI + ik1a / 6 + ik2a / 3 + ik3a / 3 + ik4a / 6
+        S_list[cnt] = S + sk1 / 6 + sk2 / 3 + sk3 / 3 + sk4 / 6
+        I_list[cnt] = I + ik1 / 6 + ik2 / 3 + ik3 / 3 + ik4 / 6
+        newI_list[cnt] = newI + ik1a / 6 + ik2a / 3 + ik3a / 3 + ik4a / 6
 
         # print(S_list[:, :, cnt])
         # print()
