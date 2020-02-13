6c191036723adf218fd520d0344e76ee89df3957
Sarah <sarah.kramer.374@gmail.com>
2020-02-13 10:31:21 +0100

    2/12: Updates to log scores, statistics, explorations
    Former-commit-id: 741eda67f1a3e7dd24e35620dee670da4bb9dbbb

--- code/figures/plot_logScores.R
+++ code/figures/plot_logScores.R
@@ -68,6 +68,213 @@ print(p1 + geom_text(data = dat.text, mapping = aes(x = -5.6, y = -0.25, label =
 ########################################################################################################################################################################
 ########################################################################################################################################################################
 
+# # Explore why PT worse for network:
+# boxplot(score ~ model + lead_mean, data = d.temp[d.temp$metric == 'Peak Timing', ])
+# # note that only the mean, not the median, is lower - suggests that there are an abundance of "failures"
+# # and gets worse with increasing lead week
+# 
+# d.temp <- d.temp[d.temp$metric == 'Peak Timing', ]
+# 
+# d.temp[d.temp$score == -10 & d.temp$season == '2012-13', ] # actually pretty few, but mostly network
+# # so is it not necessarily that there are a lot of -10s, just that there are a lot of really low values in general for the network?
+# # for network, mostly HU and LU - could be missing observed peaks b/c of noisiness?
+# 
+# # remove LU and look again:
+# d.noLU <- d.temp[d.temp$country != 'LU', ]
+# 
+# d.agg <- aggregate(score ~ lead_mean + model, data = d.noLU, FUN = mean)
+# d.agg.count <- aggregate(score ~ lead_mean + model, data = d.noLU, FUN = length)
+# d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model')); rm(d.agg.count)
+# names(d.agg)[3:4] <- c('score', 'count')
+# 
+# p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score)) + geom_line(aes(col = model)) + geom_point(aes(col = model, size = count)) +
+#   theme_classic() +
+#   scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+#   scale_y_continuous(limits = c(-5, 0), breaks = -10:0) +
+#   scale_size_continuous(breaks = c(10, 100, 300, 800), labels = c(10, 100, 300, 800),
+#                         limits = c(1, 900), range = c(1,6)) +
+#   labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model:', size = '# of Fcasts:') +
+#   guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+# print(p1)
+# # difference is still there; pretty similar to before
+# 
+# d.noLU <- d.temp[d.temp$country != 'HU' & d.temp$country != 'IT' & d.temp$country != 'ES' & d.temp$country != 'PL', ]
+# 
+# d.agg <- aggregate(score ~ lead_mean + model, data = d.noLU, FUN = mean)
+# d.agg.count <- aggregate(score ~ lead_mean + model, data = d.noLU, FUN = length)
+# d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model')); rm(d.agg.count)
+# names(d.agg)[3:4] <- c('score', 'count')
+# 
+# p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score)) + geom_line(aes(col = model)) + geom_point(aes(col = model, size = count)) +
+#   theme_classic() +
+#   scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+#   scale_y_continuous(limits = c(-5, 0), breaks = -10:0) +
+#   scale_size_continuous(breaks = c(10, 100, 300, 800), labels = c(10, 100, 300, 800),
+#                         limits = c(1, 900), range = c(1,6)) +
+#   labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model:', size = '# of Fcasts:') +
+#   guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+# print(p1)
+# # again, still there - so it's not consistently enough just these countries
+# rm(d.noLU)
+# # see above - now very little difference before predicted peak
+# 
+# # check by season:
+# d.agg <- aggregate(score ~ lead_mean + model + season, data = d.temp, FUN = mean)
+# d.agg.count <- aggregate(score ~ lead_mean + model + season, data = d.temp, FUN = length)
+# d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model', 'season')); rm(d.agg.count)
+# names(d.agg)[4:5] <- c('score', 'count')
+# 
+# p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score)) + geom_line(aes(col = model)) + geom_point(aes(col = model, size = count)) +
+#   theme_classic() +
+#   facet_wrap(~ season, scales = 'free') + scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+#   scale_y_continuous(limits = c(-5, 0), breaks = -10:0) +
+#   scale_size_continuous(breaks = c(10, 100, 300, 800), labels = c(10, 100, 300, 800),
+#                         limits = c(1, 900), range = c(1,6)) +
+#   labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model:', size = '# of Fcasts:') +
+#   guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+# print(p1)
+# 
+# # the difference after the predicted peak is pretty consistent - suggests an issue with fits almost?
+# # biggest consistent difference for 2013-14, 2016-17, 2017-18
+# 
+# # check by subtype:
+# d.agg <- aggregate(score ~ lead_mean + model + subtype, data = d.temp, FUN = mean)
+# d.agg.count <- aggregate(score ~ lead_mean + model + subtype, data = d.temp, FUN = length)
+# d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model', 'subtype')); rm(d.agg.count)
+# names(d.agg)[4:5] <- c('score', 'count')
+# 
+# p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score)) + geom_line(aes(col = model)) + geom_point(aes(col = model, size = count)) +
+#   theme_classic() +
+#   facet_wrap(~ subtype, scales = 'free') + scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+#   scale_y_continuous(limits = c(-5, 0), breaks = -10:0) +
+#   scale_size_continuous(breaks = c(10, 100, 300, 800), labels = c(10, 100, 300, 800),
+#                         limits = c(1, 900), range = c(1,6)) +
+#   labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model:', size = '# of Fcasts:') +
+#   guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+# print(p1)
+# # different after peak for all, but only before peak for H3
+# 
+# # where -10?:
+# d.low <- d.temp[d.temp$score == -10, ] # 832 / 14,202
+# 
+# table(d.low$model) # 597 network, 235 isolated
+# table(d.low$model, d.low$season)
+# table(d.low$model, d.low$lead_mean) # always more in network, but amount more doesn't always increase over time
+# table(d.low$subtype, d.low$lead_mean, d.low$model) # H3 worst, but all but early B have more -10 in network
+# table(d.low$country, d.low$model) # not consistently one country - generally more issues with network than isolated
+# # but LU has a lot, as do DE, BE, and HU
+# table(d.low$country, d.low$lead_mean, d.low$model) # there's just more everywhere, I don't think we can attribute this to a single country
+# # so we can attribute the differences to this small amount of failures, but not a consistent spot where they happen?
+# table(d.low$model, d.low$fc_start)
+# 
+# # check by country:
+# d.agg <- aggregate(score ~ lead_mean + model + country, data = d.temp, FUN = mean)
+# d.agg.count <- aggregate(score ~ lead_mean + model + country, data = d.temp, FUN = length)
+# d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model', 'country')); rm(d.agg.count)
+# names(d.agg)[4:5] <- c('score', 'count')
+# 
+# p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score)) + geom_line(aes(col = model)) + geom_point(aes(col = model, size = count)) +
+#   theme_classic() +
+#   facet_wrap(~ country, scales = 'free') + scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+#   scale_y_continuous(limits = c(-5, 0), breaks = -10:0) +
+#   scale_size_continuous(breaks = c(10, 100, 300, 800), labels = c(10, 100, 300, 800),
+#                         limits = c(1, 900), range = c(1,6)) +
+#   labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model:', size = '# of Fcasts:') +
+#   guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+# print(p1)
+# # really bad over all times for ES, HU, IT, (LU from -2 on), PL (SK?)
+# # actually better or no real difference for AT, CZ, NL
+# 
+# # try after removing -10:
+# d.notlow <- d.temp[d.temp$score > -10, ]
+# 
+# d.agg <- aggregate(score ~ lead_mean + model, data = d.notlow, FUN = mean)
+# d.agg.count <- aggregate(score ~ lead_mean + model, data = d.notlow, FUN = length)
+# d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model')); rm(d.agg.count)
+# names(d.agg)[3:4] <- c('score', 'count')
+# 
+# p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score)) + geom_line(aes(col = model)) + geom_point(aes(col = model, size = count)) +
+#   theme_classic() +
+#   scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+#   scale_y_continuous(limits = c(-5, 0), breaks = -10:0) +
+#   scale_size_continuous(breaks = c(10, 100, 300, 800), labels = c(10, 100, 300, 800),
+#                         limits = c(1, 900), range = c(1,6)) +
+#   labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model:', size = '# of Fcasts:') +
+#   guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+# print(p1)
+# # almost no difference
+# # so it seems to be these -10s - note that without them, the median score is -1.6261 - so -10 will really bring the mean down
+# 
+# par(mfrow = c(2, 1))
+# hist(d.notlow$score[d.notlow$model == 'Network'])
+# hist(d.notlow$score[d.notlow$model == 'Isolated'])
+# 
+# summary(d.notlow$score[d.notlow$model == 'Network'])
+# summary(d.notlow$score[d.notlow$model == 'Isolated'])
+# # so distributions are pretty similar (isolated has less variance, but same mean/median) when -10s are removed
+# 
+# ### So we can attribute the difference in means to a tendency to more "failure" with the network model than the isolated ###
+# 
+# # Do the forecasts with failure have equivalent isolated forecasts, or is the difference b/c of tendency to predict earlier onset forecasts?
+# d.low$group <- paste(d.low$season, d.low$country, d.low$fc_start, d.low$subtype, d.low$lead_mean, sep = '_')
+# d.low$group <- factor(d.low$group)
+# 
+# d.temp$group <- paste(d.temp$season, d.temp$country, d.temp$fc_start, d.temp$subtype, d.temp$lead_mean, sep = '_')
+# d.temp$group <- factor(d.temp$group)
+# 
+# for (i in levels(d.low2$group)) {
+#   print(i)
+#   print(as.vector(unique(d.temp$model[d.temp$group == i])))
+# }
+# # mostly these ARE forecasts that exist for both the network and isolated models, so it's not just a tendency to produce earlier forecasts
+# # wait, but not at the same lead! -- many actually do have both, though
+# 
+# # can find a single/or a couple spots where this failure is happening, and investigate those to get some idea of what is happening overall
+# d.low2 <- d.low[d.low$subtype == 'A(H3)' & d.low$lead_mean %in% c(-3, 3), ]
+# d.low2$group <- factor(d.low2$group)
+# levels(d.low2$group) # a couple have both - DE/55, HU/55, HU/60, HU/64
+# # choose one before and one after maybe
+# # HU/55/16-17 and HU/64/2012-13 (both have isolated forecasts at same predicted lead and everything)
+# # first: predicted lead -3, observed lead -1; second: predicted lead 3, observed lead 4
+# d.full1 <- read.csv('results/by_subtype/network_A(H3)/original/outputDist_A(H3)_testSet1.csv')
+# d.full2 <- read.csv('results/by_subtype/isolated_A(H3)/original/outputDist_A(H3)_ISOLATED_update.csv')
+# 
+# d.full1 <- d.full1[d.full1$country == 'HU' & d.full1$season == '2016-17' & d.full1$fc_start == 55 & d.full1$metric == 'pw', ]
+# d.full1 <- d.full1[d.full1$value > 0, ]
+# d.full2 <- d.full2[d.full2$country == 'HU' & d.full2$season == '2016-17' & d.full2$fc_start == 55 & d.full2$metric == 'pw', ]
+# d.full2 <- d.full2[d.full2$value > 0, ]
+# # obs pkwk 56
+# d.full1$bin <- d.full1$bin + 40 - 1
+# d.full2$bin <- d.full2$bin + 40 - 1
+# # by this point 6/12 countries have already peaked - would expect the model to bring things down, but seems to want to forecast peak 2 weeks later
+# # HU has the highest scaled peak this season
+# 
+# # main point might just be that - yes, this would be less accurate even if we widened the window, b/c isolated tend to be 1 rather than 2 weeks late; but it's assigning
+# # the value of -10 to forecasts where 0 vs 1 (score = -5.7) ensemble members are in the right 1-week window
+#     # isolated tend to assign ~14 to correct bin
+# # but these forecasts have the same mean prediction
+# 
+# # By season and subtype - is the effect only seen for some combinations?:
+# # check by season:
+# d.agg <- aggregate(score ~ lead_mean + model + season + subtype, data = d.temp, FUN = mean)
+# d.agg.count <- aggregate(score ~ lead_mean + model + season + subtype, data = d.temp, FUN = length)
+# d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model', 'season', 'subtype')); rm(d.agg.count)
+# names(d.agg)[4:5 + 1] <- c('score', 'count')
+# 
+# p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score)) + geom_line(aes(col = model)) + geom_point(aes(col = model, size = count)) +
+#   theme_classic() +
+#   facet_grid(subtype ~ season, scales = 'free') + scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+#   scale_y_continuous(limits = c(-10, 0), breaks = -10:0) +
+#   scale_size_continuous(breaks = c(10, 100, 300, 800), labels = c(10, 100, 300, 800),
+#                         limits = c(1, 900), range = c(1,6)) +
+#   labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model:', size = '# of Fcasts:') +
+#   guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+# print(p1)
+
+########################################################################################################################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+
 ### Look at where onset accuracy improved ###
 d.temp <- d[d$lead_mean >= -6 & d$lead_mean < 0 & !is.na(d$leadonset5) & d$metric == 'Onset Timing', ] # 444 observations
 
@@ -93,6 +300,9 @@ table(d.temp$country[d.temp$lead_mean == -1], d.temp$model[d.temp$lead_mean == -
 # also looks like lead one has a lot more predictions especially for later seasons (2015-16, 2016-17, 2017-18)s
 
 # What would this plot look like if we limited to where forecasts at same season/country/subtype/fc_start available?:
+
+
+
 
 
 
@@ -478,6 +688,86 @@ p2 <- ggplot(data = d.agg, aes(x = FWeek, y = score, col = model)) + geom_line()
   labs(x = 'Observed Lead Week', y = 'Mean Log Score', col = 'Model', size = '# of Fcasts') +
   guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
 print(p2)
+
+# By region?
+m <- read.csv('results/network/outputMet_pro_PROC.csv')
+m <- unique(m[, c(1, 8, 48:49, 57)])
+d <- merge(d, m, by = c('season', 'country', 'subtype'))
+
+d.temp <- d[d$lead_mean >= -6 & d$lead_mean < 5 & !is.na(d$leadonset5), ]
+d.agg <- aggregate(score ~ lead_mean + model + metric + region, data = d.temp, FUN = mean)
+d.agg.count <- aggregate(score ~ lead_mean + model + metric + region, data = d.temp, FUN = length)
+d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model', 'metric', 'region')); rm(d.agg.count)
+names(d.agg)[5:6] <- c('score', 'count')
+
+p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score, col = model)) + geom_line() + geom_point(aes(size = count)) +
+  theme_bw() +
+  facet_grid(region~ metric, scales = 'free') + scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+  scale_y_continuous(limits = c(-6, 0), breaks = -10:0) +
+  scale_size_continuous(breaks = c(10, 100, 300), labels = c(10, 100, 300),
+                        limits = c(1, 650), range = c(1,6)) +
+  labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model', size = '# of Fcasts') +
+  guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+print(p1)
+
+d.temp <- d[d$FWeek >= -6 & d$FWeek < 5, ]
+d.agg <- aggregate(score ~ FWeek + model + metric + region, data = d.temp, FUN = mean)
+d.agg.count <- aggregate(score ~ FWeek + model + metric + region, data = d.temp, FUN = length)
+d.agg <- merge(d.agg, d.agg.count, by = c('FWeek', 'model', 'metric', 'region')); rm(d.agg.count)
+names(d.agg)[5:6] <- c('score', 'count')
+
+p2 <- ggplot(data = d.agg, aes(x = FWeek, y = score, col = model)) + geom_line() + geom_point(aes(size = count)) +
+  theme_bw() +
+  facet_grid(region ~ metric, scales = 'free') + scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+  scale_y_continuous(limits = c(-6, 0), breaks = -10:0) +
+  scale_size_continuous(breaks = c(10, 100, 300), labels = c(10, 100, 300),
+                        limits = c(1, 650), range = c(1,6)) +
+  labs(x = 'Observed Lead Week', y = 'Mean Log Score', col = 'Model', size = '# of Fcasts') +
+  guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+print(p2)
+# southwest a bit better than eastern, but not notable really
+
+# And by scaling range?:
+d.temp <- d[d$lead_mean >= -6 & d$lead_mean < 5 & !is.na(d$leadonset5), ]
+d.agg <- aggregate(score ~ lead_mean + model + metric + scaling.range, data = d.temp, FUN = mean)
+d.agg.count <- aggregate(score ~ lead_mean + model + metric + scaling.range, data = d.temp, FUN = length)
+d.agg <- merge(d.agg, d.agg.count, by = c('lead_mean', 'model', 'metric', 'scaling.range')); rm(d.agg.count)
+names(d.agg)[5:6] <- c('score', 'count')
+
+p1 <- ggplot(data = d.agg, aes(x = lead_mean, y = score, col = model)) + geom_line() + geom_point(aes(size = count)) +
+  theme_bw() +
+  facet_grid(scaling.range~ metric, scales = 'free') + scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+  scale_y_continuous(limits = c(-7, 0), breaks = -10:0) +
+  scale_size_continuous(breaks = c(10, 100, 300), labels = c(10, 100, 300),
+                        limits = c(1, 275), range = c(1,6)) +
+  labs(x = 'Predicted Lead Week', y = 'Mean Log Score', col = 'Model', size = '# of Fcasts') +
+  guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+print(p1)
+
+d.temp <- d[d$FWeek >= -6 & d$FWeek < 5, ]
+d.agg <- aggregate(score ~ FWeek + model + metric + scaling.range, data = d.temp, FUN = mean)
+d.agg.count <- aggregate(score ~ FWeek + model + metric + scaling.range, data = d.temp, FUN = length)
+d.agg <- merge(d.agg, d.agg.count, by = c('FWeek', 'model', 'metric', 'scaling.range')); rm(d.agg.count)
+names(d.agg)[5:6] <- c('score', 'count')
+
+p2 <- ggplot(data = d.agg, aes(x = FWeek, y = score, col = model)) + geom_line() + geom_point(aes(size = count)) +
+  theme_bw() +
+  facet_grid(scaling.range ~ metric, scales = 'free') + scale_color_brewer(palette = 'Set1') + scale_x_continuous(breaks = -8:4) +
+  scale_y_continuous(limits = c(-7, 0), breaks = -10:0) +
+  scale_size_continuous(breaks = c(10, 100, 300), labels = c(10, 100, 300),
+                        limits = c(1, 275), range = c(1,6)) +
+  labs(x = 'Observed Lead Week', y = 'Mean Log Score', col = 'Model', size = '# of Fcasts') +
+  guides(colour = guide_legend(order = 1), size = guide_legend(order = 2))
+print(p2)
+# highest scalings worst for PT, but no consistent pattern over all groups; no patterns for OT, except that maybe more forecasts for higher scalings?
+# but highest group is literally just LU
+# try regrouping:
+d$scaling.range <- as.character(d$scaling.range)
+d$scaling.range[d$scaling.range %in% c('(0,0.5]', '(0.5,1]')] <- '(0,1]'
+d$scaling.range[d$scaling.range %in% c('(2,10]', '(10,20]')] <- '(2,20]'
+d$scaling.range[d$scaling.range %in% c('(20,50]', '(50,100]')] <- '(20,100]'
+d$scaling.range <- factor(d$scaling.range)
+# no clear patterns; fewer fcasts of OT for smallest scalings (DE, FR, SK)
 
 # Do country plots in another file, but quick check:
 d.temp <- d[d$lead_mean >= -6 & d$lead_mean < 5 & !is.na(d$leadonset5), ]
